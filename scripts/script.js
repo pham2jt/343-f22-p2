956e8e2406c3b39db82057f7d0d88f78d7886096
Jess Pham <93550337+pham2jt@users.noreply.github.com>
2022-12-09 15:46:21 -0500

    testing

---
+++ scripts/script.js
@@ -0,0 +1,91 @@
+
+// get the input elements
+const searchInput = document.getElementById("search");
+const searchForm = document.getElementById("searchForm");
+const spotifyClientID = "11de459dc28144f99f45c07274449388";
+const spotifyClientSecret = "e992dfa997bb455aa83fe3d4446c9092";
+const lyricApiKey = "90f58a0ddd188f462867b24d92f70aee";
+// get the div element with the ID "results"
+const resultsDiv = document.getElementById("results");
+
+fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    body: 'grant_type=client_credentials&client_id=' + spotifyClientID + '&client_secret=' + spotifyClientSecret,
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+})
+.then(r => r.json())
+.then(r => {
+  console.log(r.access_token)
+  let spotifyApiKey = r.access_token;
+  // add an event listener for the submit event
+  searchForm.addEventListener("submit", event => {
+  // prevent the default form submission behavior
+  event.preventDefault();
+  const query = searchInput.value;
+  // perform the search for the artist's top songs and retrieve the lyrics
+  searchForArtistTopSongs(spotifyApiKey, query);
+});
+})
+
+// define the function to search for the artist's top songs and retrieve the lyrics
+function searchForArtistTopSongs(spotifyApiKey, searchQuery) {
+  // use the Spotify API to search for the artist's top songs
+  console.log(searchQuery);
+  fetch(`https://api.spotify.com/v1/search?q=${searchQuery}&type=artist&limit=1`, {
+    headers: {
+      Authorization: `Bearer ${spotifyApiKey}`
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+      // log the response data in the console
+      console.log(data);
+
+      // get the artist's ID from the response data
+      const artistName = data.artists.items[0].name;
+      const artistId = data.artists.items[0].id;
+
+      // use the Spotify API to get the artist's top songs
+      fetch(`https://api.spotify.com/v1/artists/${artistId}/top-tracks?country=US`, {
+        headers: {
+          Authorization: `Bearer ${spotifyApiKey}`
+        }
+      })
+        .then(response => response.json())
+        .then(data => {
+          // log the response data in the console
+          console.log(data);
+
+          // get the song IDs from the response data
+          const songNames = data.tracks.map(track => track.name);
+          const wordCounts = new Map();
+          // use the Rap Genius API to get the lyrics for the artist's top songs
+          songNames.forEach(songName => {
+            fetch(`matcher.lyrics.get?q_track=${songName}&q_artist=${artistName}&apikey=${lyricApiKey}`)
+              .then(response => response.json())
+              .then((lyrics) => {
+                console.log(lyrics);
+                lyrics.message.body.lyrics.lyrics_body.split(" ").forEach(word => {
+                  // increment the word count in the map
+                  wordCounts.set(word, (wordCounts.get(word) || 0) + 1);
+                });
+              })
+              .catch(error => {
+                console.log("No lyrics found for this track");
+              })
+          })
+          
+
+              // sort the word counts by the most common words
+              const sortedWordCounts = [...wordCounts.entries()].sort((a, b) => b[1] - a[1]);
+              
+              // create a list of the most common words and their counts
+              const wordList = sortedWordCounts.map(entry => `${entry[0]}: ${entry[1]}`);
+
+              // set the inner HTML of the results div to the list of words and counts
+              resultsDiv.innerHTML = wordList.join("<br>");
+            });
+        });
+    };
